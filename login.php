d9ea4d460cbaf9e655c1e22dd2131bc8a94a472e
Travis Lange <travislange12@gmail.com>
2019-03-13 22:19:04 -0400

    Added the ability to log in and out

--- login.php
+++ login.php
@@ -1,30 +1,37 @@
 <?php
+session_start();
 require "database.php";
+
+if ($_GET)
+    $errorMessage = $_GET["errorMessage"];
+else
+    $errorMessage='';
+
 if ($_POST){
     $success = false;
     $username = $_POST['username'];
-    $password = $_POST['password'];
-
+    $password = MD5 ($_POST['password']);
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "SELECT * FROM customers WHERE email='$username' AND password_hash ='temp'";
+    $sql = "SELECT * FROM customers WHERE email='$username' AND password_hash ='$password' LIMIT 1";
     $q = $pdo -> prepare($sql);
     $q -> execute(array());
     $data = $q->fetch(PDO::FETCH_ASSOC);
-    print_r($data); exit();
-    if ($success) {
+    if ($data) {
         $_SESSION["username"] = $username;
 
-        header("Location: success.php?id=$sessionid");
+        header("Location: success.php");
     } else
-        header("Location: login.php");
+        header("Location: login.php?errorMessage=Invalid");
 }
 ?>
 
 <h1>Log In</h1>
 <form class="form-horizontal" action="login.php" method="post">
+    <p style="color: red;"><?php echo($errorMessage); ?></p>
     <input name="username" type="text" placeholder="me@email.com" required>
     <input name="password" type="password" placeholder="password" required>
     <button type="submit" class="btn btn-success">Sign In</button>
+    <a href="logout.php">Sign Out</a>
 </form>
