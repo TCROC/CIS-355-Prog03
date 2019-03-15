0fa2d70356589cc7510f8dfa75c509ac7c41ceb6
Travis Lange <travislange12@gmail.com>
2019-03-14 20:19:25 -0400

    Users can now join and create accounts

--- login.php
+++ login.php
@@ -8,26 +8,31 @@ else
     $errorMessage='';
 
 if ($_POST){
-    $success = false;
+    // Get the username and password from the post.
     $username = $_POST['username'];
     $password = MD5 ($_POST['password']);
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // See if this username / password combination exists in the database.
     $sql = "SELECT * FROM customers WHERE email='$username' AND password_hash ='$password' LIMIT 1";
     $q = $pdo -> prepare($sql);
     $q -> execute(array());
     $data = $q->fetch(PDO::FETCH_ASSOC);
+
+    // If we got data back, the username / password existed.
     if ($data) {
         $_SESSION["username"] = $username;
 
+        // Go to the customer page.
         header("Location: customer.php");
-    } else
+    } else // Otherwise, try to log in again.
         header("Location: login.php?errorMessage=Invalid Username or Password!");
 }
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset='UTF-8'>
     <script src=\"https://code.jquery.com/jquery-3.3.1.min.js\"
@@ -44,8 +49,9 @@ if ($_POST){
         <input name="username" type="text" placeholder="me@email.com" required>
         <input name="password" type="password" placeholder="password" required>
         <button type="submit" class="btn btn-success">Sign In</button>
-        <a href="createAccount.php" class="btn btn-info">Create Account</a>
+        <a href="createAccount.php" class="btn btn-info">Join</a>
         <?php
+        // Displays an error message if there is one.
         if ($errorMessage) {
             echo "<p class=\"alert alert-danger\" role=\"alert\">$errorMessage</p>";
         }
